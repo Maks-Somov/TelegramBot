2d02d51858347ac24894a338e3be89fbe6d372e3
Maks-Somov <fail.epic2015@yandex.ru>
2020-02-26 21:21:29 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+
+public class Main {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();//инициализациия API telegram
+        
+    }
+}
