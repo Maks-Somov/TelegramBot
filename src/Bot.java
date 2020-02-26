1a4b74a6138f61ffb4191e3bccf377d2764debd0
Maks-Somov <fail.epic2015@yandex.ru>
2020-02-26 21:41:17 +0300

    create bot and simple send message

--- src/Bot.java
+++ src/Bot.java
@@ -1,2 +1,30 @@
-public class Bot {
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+public class Bot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+        SendMessage sendMessage = new SendMessage().setChatId(update.getMessage().getChatId());//class for send message
+        if(update.getMessage().getText().equals("hello")){
+            sendMessage.setText("hi");
+            try{
+                execute(sendMessage);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "MyBotOnJava";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "1116726600:AAE4GBI5mA9k4dpWA2hv_dHKW4dj9LTHJRc";
+    }
 }
