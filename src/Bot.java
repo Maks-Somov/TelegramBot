23d843e6ed9de92f746a4c062af0332cbc695a57
Maks-Somov <fail.epic2015@yandex.ru>
2020-02-27 10:56:26 +0300

    upadte Bot

--- src/Bot.java
+++ src/Bot.java
@@ -1,7 +1,13 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
 
 
 public class Bot extends TelegramLongPollingBot {
@@ -26,7 +32,40 @@ public class Bot extends TelegramLongPollingBot {
         if(msg.contains("hi") || msg.contains("hello")){
             return "hi guy";
         }
+        if(msg.contains("Информация о книге")){
+            return getInfoBook();
+        }
         return msg;
+    }
+
+    public String getInfoBook(){
+        try {
+            URL url = new URL(book.getImage());
+            // берем сслыку на изображение
+            BufferedImage img = ImageIO.read(url);
+            // качаем изображение в буфер
+            File outputfile = new File("image.jpg");
+            //создаем новый файл в который поместим
+            //скаченое изображение
+            ImageIO.write(img, "jpg", outputfile);
+            //преобразовуем наше буферное изображение
+            //в новый файл
+            SendPhoto sendPhoto = new SendPhoto().setChatId(chat_id);
+            sendPhoto.setPhoto(outputfile);
+            execute(sendPhoto);
+        } catch (Exception e){
+            System.out.println("File not found");
+            e.printStackTrace();
+        }
+
+        String info = book.getTitle()
+                + "\nАвтор" + book.getAutorName()
+                + "\nЖанр" + book.getGenres()
+                + "\n\nОписание\n" + book.getDescription()
+                + "\n\nКоличество лайков " + book.getLikes()
+                + "\n\nПоследние комментарии\n" + book.getCommentList();
+
+        return info;
     }
 
     @Override
